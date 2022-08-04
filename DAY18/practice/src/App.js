b4693ffcb4fc1d30a26790a49b419ac9c99f32ed
rymzmean <103475193+rymzmean@users.noreply.github.com>
2022-08-04 12:56:24 +0900

    0804

---
+++ DAY18/practice/src/App.js
@@ -0,0 +1,13 @@
+import "./reset.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>H1 메인 타이틀입니다</h1>
+      <p className="hi">이곳은 p태그 영역입니다. 안녕하세요</p>
+    </div>
+  );
+}
+
+export default App;
